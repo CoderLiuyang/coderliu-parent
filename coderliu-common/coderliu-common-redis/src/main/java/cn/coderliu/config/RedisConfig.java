eabffff2482ccba2728e828a2baade8561520584
liuyang <popoliuyang@qq.com>
2021-12-20 21:46:02 +0800

    增加oauth服务

--- coderliu-common/coderliu-common-redis/src/main/java/cn/coderliu/config/RedisConfig.java
+++ coderliu-common/coderliu-common-redis/src/main/java/cn/coderliu/config/RedisConfig.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
---
+++ coderliu-oauthserver/src/main/java/cn/coderliu/CoderliuOauthserverApplication.java
@@ -0,0 +1,13 @@
+package cn.coderliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderliuOauthserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderliuOauthserverApplication.class, args);
+    }
+
+}
