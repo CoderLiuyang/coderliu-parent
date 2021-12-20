283334633a8a16197bad628deca03f08f5b03419
liuyang <popoliuyang@qq.com>
2021-12-20 22:06:50 +0800

    调整项目结构

--- coderliu-common/coderliu-common-db/src/main/java/cn/coderliu/config/MybatisPlusConfig.java
+++ coderliu-common/coderliu-common-db/src/main/java/cn/coderliu/config/MybatisPlusConfig.java
@@ -1,7 +1,9 @@
 package cn.coderliu.config;
 
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,15 +14,15 @@ import org.springframework.context.annotation.Configuration;
 public class MybatisPlusConfig {
 
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
         // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
-        // paginationInterceptor.setOverflow(false);
+        paginationInnerInterceptor.setOverflow(false);
         // 设置最大单页限制数量，默认 500 条，-1 不受限制
-        paginationInterceptor.setLimit(-1);
-        // 开启 count 的 join 优化,只针对部分 left join
-//        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
-        return paginationInterceptor;
+        paginationInnerInterceptor.setMaxLimit(-1L);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return interceptor;
     }
 
 }
