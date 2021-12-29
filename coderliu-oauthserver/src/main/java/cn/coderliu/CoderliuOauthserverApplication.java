d24291ac5481aa963a9374e0b413677741d47078
liuyang <popoliuyang@qq.com>
2021-12-29 11:49:17 +0800

    调整项目结构

--- coderliu-oauthserver/src/main/java/cn/coderliu/CoderliuOauthserverApplication.java
+++ coderliu-oauthserver/src/main/java/cn/coderliu/CoderliuOauthserverApplication.java
@@ -2,9 +2,9 @@ package cn.coderliu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableFeignClients
+@EnableFeignClients
 @SpringBootApplication
 public class CoderliuOauthserverApplication {
 
