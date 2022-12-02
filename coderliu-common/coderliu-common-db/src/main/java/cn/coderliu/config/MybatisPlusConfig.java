2ee536a006df32507081f109e10ab1fc670b702c
liuyang <popoliuyang@qq.com>
2022-12-02 09:31:43 +0800

    修改mybatis分页设置

--- coderliu-common/coderliu-common-db/src/main/java/cn/coderliu/config/MybatisPlusConfig.java
+++ coderliu-common/coderliu-common-db/src/main/java/cn/coderliu/config/MybatisPlusConfig.java
@@ -7,30 +7,14 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
-
 @Configuration
 public class MybatisPlusConfig {
 
-    /**
-     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
-     */
-
     @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor(){
-        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
-        mybatisPlusInterceptor.setInterceptors(Collections.singletonList(paginationInnerInterceptor()));
-        return mybatisPlusInterceptor;
-    }
-    @Bean
-    public PaginationInnerInterceptor paginationInnerInterceptor() {
-        PaginationInnerInterceptor paginationInterceptor = new PaginationInnerInterceptor();
-        // 设置最大单页限制数量，默认 500 条，-1 不受限制
-        paginationInterceptor.setMaxLimit(-1L);
-        paginationInterceptor.setDbType(DbType.MYSQL);
-        // 开启 count 的 join 优化,只针对部分 left join
-        paginationInterceptor.setOptimizeJoin(true);
-        return paginationInterceptor;
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 
 }
