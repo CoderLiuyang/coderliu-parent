1fbfe8561f5915b49e545153b472d99e860555e1
liuyang <popoliuyang@qq.com>
2022-12-23 23:14:55 +0800

    update增加spring-cache集成redis

--- coderliu-common/coderliu-common-redis/src/main/java/cn/coderliu/config/JsonMapperRedisConfig.java
+++ coderliu-common/coderliu-common-redis/src/main/java/cn/coderliu/config/JsonMapperRedisConfig.java
@@ -5,22 +5,35 @@ import cn.coderliu.serializer.FastJson2JsonRedisSerializer;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 
 /**
  * @author liu
  */
-public class JsonMapperRedisConfig {
+@EnableCaching
+@RequiredArgsConstructor
+public class JsonMapperRedisConfig implements CachingConfigurer {
+
+    private final RedisConnectionFactory factory;
+    private FastJson2JsonRedisSerializer serializer = new FastJson2JsonRedisSerializer(Object.class);
 
 
     @Bean(name = "jsonMapperRedisTemplate")
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+    public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
-        FastJson2JsonRedisSerializer serializer = new FastJson2JsonRedisSerializer(Object.class);
+
         ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         serializer.setObjectMapper(mapper);
@@ -30,4 +43,18 @@ public class JsonMapperRedisConfig {
         template.afterPropertiesSet();
         return template;
     }
+
+    /**
+     * 缓存管理器
+     * -> 使用redis做cache缓存注解管理
+     * -> 修改原生的序列化方式改为json方式
+     */
+    @Override
+    public CacheManager cacheManager() {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+        return RedisCacheManager.builder(factory).cacheDefaults(cacheConfiguration).build();
+    }
+
 }
--- coderliu-gateway/src/main/java/cn/coderliu/filter/GateWayFilter.java
+++ coderliu-gateway/src/main/java/cn/coderliu/filter/GateWayFilter.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
