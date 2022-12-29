9d08238c2454764455170524348a4199feaf2a4f
liuyang <popoliuyang@qq.com>
2022-12-29 08:48:01 +0800

    增加工作流的模块

---
+++ coderliu-workflow/src/main/java/cn/coderliu/CoderliuWorkflowApplication.java
@@ -0,0 +1,17 @@
+package cn.coderliu;
+
+import cn.coderliu.annotation.EnableResourceServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableResourceServer
+public class CoderliuWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderliuWorkflowApplication.class, args);
+    }
+
+}
