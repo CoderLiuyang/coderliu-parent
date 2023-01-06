a9cf02ad65dcc67de873b8dbc04f99ddf72002a4
liuyang <popoliuyang@qq.com>
2023-01-06 09:33:50 +0800

    update 增加文件模块

---
+++ coderliu-fileserver/src/main/java/cn/coderliu/CoderliuFileserverApplication.java
@@ -0,0 +1,15 @@
+package cn.coderliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class CoderliuFileserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderliuFileserverApplication.class, args);
+    }
+
+}
