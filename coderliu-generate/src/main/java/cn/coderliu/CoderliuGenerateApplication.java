16212eb299609bb301de1f50cec681e458713c49
liuyang <popoliuyang@qq.com>
2023-05-12 15:30:46 +0800

    update解决pom中的打包问题

--- coderliu-generate/src/main/java/cn/coderliu/CoderliuGenerateApplication.java
+++ coderliu-generate/src/main/java/cn/coderliu/CoderliuGenerateApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+
 @EnableFeignClients
 @EnableResourceServer
+@SpringBootApplication
 public class CoderliuGenerateApplication {
 
     public static void main(String[] args) {
