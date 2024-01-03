664d502c7f271c142e88f7cb7b4687dc0767000a
liuyang <popoliuyang@qq.com>
2024-01-03 21:08:21 +0800

    del activiti模块

--- coderliu-activiti/src/main/java/cn/coderliu/mapper/BizLeaveMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.coderliu.mapper;
-
-import cn.coderliu.model.BizLeave;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface BizLeaveMapper extends BaseMapper<BizLeave> {
-}
