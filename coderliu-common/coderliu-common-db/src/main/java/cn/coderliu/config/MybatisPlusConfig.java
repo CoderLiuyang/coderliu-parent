f67b51db2b32f84f3478ec015f12174ef880e1fd
liuyang <popoliuyang@qq.com>
2025-01-04 20:46:54 +0800

    update mybatis-plus version

--- coderliu-common/coderliu-common-db/src/main/java/cn/coderliu/config/MybatisPlusConfig.java
+++ coderliu-common/coderliu-common-db/src/main/java/cn/coderliu/config/MybatisPlusConfig.java
@@ -2,7 +2,6 @@ package cn.coderliu.config;
 
 
 import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
