ce99a4f7a64107461a87ce7a554161847996965a
liuyang <popoliuyang@qq.com>
2025-01-04 20:47:17 +0800

    update mybatis-plus version

--- coderliu-common/coderliu-common-security/src/main/java/cn/coderliu/component/CustomSecurityMessageSourceConfiguration.java
+++ coderliu-common/coderliu-common-security/src/main/java/cn/coderliu/component/CustomSecurityMessageSourceConfiguration.java
@@ -1,31 +1,31 @@
-//
-//
-//package cn.coderliu.component;
-//
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-//import org.springframework.context.MessageSource;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import java.util.Locale;
-//import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.SERVLET;
-//
-//
-///**
-// *
-// * 国际化配置
-// * 注入自定义错误处理,覆盖 org/springframework/security/messages 内置异常
-// */
-//@ConditionalOnWebApplication(type = SERVLET)
-//public class CustomSecurityMessageSourceConfiguration implements WebMvcConfigurer {
-//
-//	@Bean
-//	public MessageSource securityMessageSource() {
-//		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-//		messageSource.addBasenames("classpath:i18n/errors/messages");
-//		messageSource.setDefaultLocale(Locale.CHINA);
-//		return messageSource;
-//	}
-//
-//}
+
+
+package cn.coderliu.component;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Locale;
+import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.SERVLET;
+
+
+/**
+ *
+ * 国际化配置
+ * 注入自定义错误处理,覆盖 org/springframework/security/messages 内置异常
+ */
+@ConditionalOnWebApplication(type = SERVLET)
+public class CustomSecurityMessageSourceConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public MessageSource securityMessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.addBasenames("classpath:i18n/errors/messages");
+		messageSource.setDefaultLocale(Locale.CHINA);
+		return messageSource;
+	}
+
+}
