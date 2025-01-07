b111128b31355c340389dec660de014de80fda3f
liuyang <popoliuyang@qq.com>
2025-01-07 21:34:39 +0800

    解决业务服务启动报错

--- coderliu-common/coderliu-common-security/src/main/java/cn/coderliu/config/ResourceServerAutoConfiguration.java
+++ coderliu-common/coderliu-common-security/src/main/java/cn/coderliu/config/ResourceServerAutoConfiguration.java
@@ -2,19 +2,26 @@ package cn.coderliu.config;
 
 import cn.coderliu.component.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 
-@RequiredArgsConstructor
 @EnableConfigurationProperties(PermitAllUrlProperties.class)
 public class ResourceServerAutoConfiguration {
 
+
+    @Resource
+    @Qualifier("securityMessageSource")
+    private MessageSource securityMessageSource;
+
     /**
      * 鉴权具体的实现逻辑
+     *
      * @return （#pms.xxx）
      */
     @Bean("pms")
@@ -24,6 +31,7 @@ public class ResourceServerAutoConfiguration {
 
     /**
      * 请求令牌的抽取逻辑
+     *
      * @param urlProperties 对外暴露的接口列表
      * @return BearerTokenExtractor
      */
@@ -34,18 +42,19 @@ public class ResourceServerAutoConfiguration {
 
     /**
      * 资源服务器异常处理
-     * @param objectMapper jackson 输出对象
+     *
+     * @param objectMapper          jackson 输出对象
      * @param securityMessageSource 自定义国际化处理器
      * @return ResourceAuthExceptionEntryPoint
      */
     @Bean
-    public ResourceAuthExceptionEntryPoint resourceAuthExceptionEntryPoint(ObjectMapper objectMapper,
-                                                                           MessageSource securityMessageSource) {
+    public ResourceAuthExceptionEntryPoint resourceAuthExceptionEntryPoint(ObjectMapper objectMapper) {
         return new ResourceAuthExceptionEntryPoint(objectMapper, securityMessageSource);
     }
 
     /**
      * 资源服务器toke内省处理器
+     *
      * @param authorizationService token 存储实现
      * @return TokenIntrospector
      */
